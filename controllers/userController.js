64787197ea73d1b0919e17b16dd4fbcaa885b1eb
dancarlton <dan@dancarlton.com>
2025-07-01 13:59:57 -0700

    added .gitignore file

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,14 +4,14 @@ const jwt = require('jsonwebtoken')
 
 exports.register = async (req, res) => {
   try {
-    const { email, password } = req.body
+    const { name, email, password } = req.body
 
     const existingUser = await User.findOne({ email })
     if (existingUser) return res.status(400).json({ message: 'User already exists' })
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    const newUser = await User.create({ email, password: hashedPassword })
+    const newUser = await User.create({ name, email, password: hashedPassword })
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
