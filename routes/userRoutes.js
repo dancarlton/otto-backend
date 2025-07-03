91c45a13492fb0dc09f898cfb9e2ae5272efa8b9
dancarlton <dan@dancarlton.com>
2025-07-02 20:15:40 -0700

    working through successfully loggin in

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { register, updateUser } = require('../controllers/userController')
+const { register, updateUser, login } = require('../controllers/userController')
 const auth = require( '../middleware/auth' )
 
+router.post('/login',login)
 router.post('/register', register)
 router.patch('/update', auth, updateUser)
 
