5005cf92bdb8e112253cc0ea3d02c08eab6ddb89
dancarlton <dan@dancarlton.com>
2025-07-03 22:39:43 -0700

    successfully logged/registers and routed to homepage / created main & preloader components

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { register, updateUser, login } = require('../controllers/userController')
+const { register, updateUser, login, getUser } = require('../controllers/userController')
 const auth = require( '../middleware/auth' )
 
 router.post('/login',login)
 router.post('/register', register)
+router.get('/me', auth, getUser)
 router.patch('/update', auth, updateUser)
 
 module.exports = router
