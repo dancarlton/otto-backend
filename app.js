ee0f3897fe490786bc8f183b705ee6632d6b287b
Dan Carlton <danlouis.carlton@gmail.com>
2025-07-07 13:59:11 -0700

    Remove .env file from repo

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// userRoutes
 const userRoutes = require('./routes/userRoutes')
 app.use('/api/users', userRoutes)
+
+// gptRoutes
+const gptRoutes = require('./routes/gptRoutes')
+app.use('/api/gpt', gptRoutes)
 
 module.exports = app
