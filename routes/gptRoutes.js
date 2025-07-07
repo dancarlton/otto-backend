abb9ec39bc809915c5fa9c2ed9d6b5b935b50b87
Dan Carlton <danlouis.carlton@gmail.com>
2025-07-07 16:04:04 -0700

    updated User schema to have travelDistance / completed ChatBox

--- routes/gptRoutes.js
+++ routes/gptRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const auth = require( '../middleware/auth' )
+const auth = require('../middleware/auth')
 const { askOtto } = require('../controllers/gptController')
 
 router.post('/chat', auth, askOtto)
