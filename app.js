e088f398e5216f71affde639ade3c3089988c7df
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-19 10:57:27 -0700

    moved health route before api mounts

--- app.js
+++ app.js
@@ -15,19 +15,17 @@ app.use(express.json());
 app.use(helmet());
 app.use(limiter);
 
-// routes
-const routes = require("./routes");
-
-app.get("/", (req, res) => {
-  res.send("Otto backend is alive");
-});
-
-app.use("/api", routes);
 
 // health is for waking up Render server
 app.get("/health", (req, res) => {
   res.json({ status: "ok", service: "otto-backend" });
 });
+
+// routes
+const routes = require("./routes");
+
+app.use("/api", routes);
+
 
 // error handler (should come after all routes)
 const errorHandler = require("./middleware/errorHandler");
